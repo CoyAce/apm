3026e97f04c55149e92d3aa87d55d1d8a0b751e7
CoyAce <AkeyCoy@gmail.com>
2026-02-04 14:13:08 +0800

    webrtc: exclude .m with cgo build tag

--- google.com/webrtc/rtc_base/system/darwin.go
+++
@@ -1,8 +0,0 @@
-//go:build darwin || ios
-
-package system
-
-// #cgo CFLAGS: -I${SRCDIR}/../..
-// #cgo CFLAGS: -x objective-c
-// #include "darwin/gcd_helpers.m"
-import "C"
--- google.com/webrtc/rtc_base/system/gcd_helpers.m
+++ google.com/webrtc/rtc_base/system/gcd_helpers.m
@@ -7,9 +7,10 @@
  *  in the file PATENTS.  All contributing project authors may
  *  be found in the AUTHORS file in the root of the source tree.
  */
+ //go:build darwin || ios
 
 #if defined(__APPLE__)
-#include "rtc_base/system/darwin/gcd_helpers.h"
+#include "rtc_base/system/gcd_helpers.h"
 
 dispatch_queue_t RTCDispatchQueueCreateWithTarget(const char *label,
                                                   dispatch_queue_attr_t attr,
